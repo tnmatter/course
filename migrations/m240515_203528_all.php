55ac560c97798d034c868ecfba68642cb9a9111c
a.gostev <gostev.a@perfluence.io>
2024-05-15 23:35:46 +0300

    init

---
+++ migrations/m240515_203528_all.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m240515_203528_all
+ */
+class m240515_203528_all extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m240515_203528_all cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m240515_203528_all cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
